de0b90a390987db44483fe5fe93b78fa9a361a17
OliverW10 <oliver.warrick2@gmail.com>
2024-01-04 21:56:13 +1100

    make ui

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Git Accuse
-### Find not only the author of the code but also who reviwed the PR and previous authors of the code
+### Chrome extension that allows you to find not only the author of the code but also who reviewed the PR and previous authors of the code
 
 Sometimes you need to find ALL responsible people and `git blame` is just not enough
--- popup/popup.js
+++ popup/popup.js
@@ -1,32 +1,77 @@
 window.onload = async ()=>{
-    let usersDiv = document.getElementById("users");
+    let messageDiv = document.getElementById("message");
+
+    const message = (msg)=>{
+        messageDiv.textContent = msg;
+    }
+
+    // should be a github page with a single line selected
     let pageUrl = await getPageUrl();
-    console.log(pageUrl);
+    
     if(pageUrl == undefined){
-        usersDiv.innerHTML = "Could not get current page url";
+        message("Could not get current page url");
+        return;
     }
     if(!pageUrl.includes("github.com")){
-        usersDiv.innerHTML = "Current page is not github";
+        message("Current page is not github");
         return;
     }
     if(!pageUrl.includes("#L")){
-        usersDiv.innerHTML = "Select a line by clicking on the line number to view blame";
+        message("Select a line by clicking on the line number to view blame");
         return;
     }
+
     let [, user, repo,, branch, ...path] = pageUrl.split("github.com")[1].split("/")
     let [file, line] = path[path.length-1].split("#L")
-    let commitHash = await getBlameCommit(user, repo, branch, path, Number(line));
+    message("Getting first blame");
+    let blame = await getBlame(user, repo, branch, path);
+    let commitHash = getLineBlameCommitHash(blame, Number(line));
+    message("Getting reviwers");
     let prNum = await getPullRequest(user, repo, commitHash);
     let reviewers = await getApprovingReviwers(user, repo, prNum);
-    let authors = await getPreviousCommit(user, repo, commitHash)
+    message("Getting previous authors");
+    let allAuthors = await getPreviousAuthors(blame, user, repo, path, commitHash, line);
+    
+    createPage(messageDiv, reviewers, allAuthors);
 };
 
+function createPage(div, reviewers, authors){
+    console.log(reviewers);
+    console.log(authors);
+    div.innerText = ``;
+    let reviewersTitle = document.createElement("h3");
+    reviewersTitle.textContent = "Responsible Reviewers";
+    div.appendChild(reviewersTitle);
+    div.appendChild(document.createElement("hr"));
+    for(let user of [...new Set(reviewers)]){
+        createRow(div, user);
+    }
 
-// async function getSelected(){
-//     return chrome.tabs.executeScript({code: "window.getSelection().toString();"})[0];
-// }
+    let authorsTitle = document.createElement("h3");
+    authorsTitle.textContent = "Responsible Authors";
+    div.appendChild(authorsTitle);
+    div.appendChild(document.createElement("hr"));
+    for(let user of [...new Set(authors)]){
+        createRow(div, user);
+    }
+}
 
-// is a github page with a single line selected
+function createRow(div, user){
+    console.log(user);
+    let rowDiv = document.createElement("div");
+    rowDiv.className = "rowDiv"
+    let img = document.createElement("img");
+    img.src = user.img;
+    rowDiv.appendChild(img);
+
+    let text = document.createElement("p");
+    text.innerText = user.name;
+    rowDiv.appendChild(text);
+
+    div.appendChild(rowDiv);
+}
+
+
 async function getPageUrl(){
     let queryOptions = { active: true, currentWindow: true };
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
@@ -35,60 +80,37 @@ async function getPageUrl(){
     return tab.url;
 }
 
-function hasLineSelected(url){
-    return ;
+async function getBlame(user, repo, branch, path){
+    let response = await fetch(`https://github.com/${user}/${repo}/blame/${branch}/${path.join("/")}`);
+    return await response.json();
 }
 
-// const blameQuery = `{
-//     # repository name/owner
-//     repository(name: "opencv", owner: "opencv") {
-//       # branch name
-//       ref(qualifiedName:"master") {
-//         target {
-//           # cast Target to a Commit
-//           ... on Commit {
-//             # full repo-relative path to blame file
-//             blame(path:"include/opencv2/opencv.hpp") {
-//               ranges {
-//                 commit {
-//                   author {
-//                     name
-//                   }
-//                 }
-//                 startingLine
-//                 endingLine
-//                 age
-//               }
-//             }
-//           }
-//         }
-//       }
-// }`
-async function getBlameCommit(user, repo, branch, path, line){
-    let response = await fetch(`https://github.com/${user}/${repo}/blame/${branch}/${path.join("/")}`);
-    let result = await response.json();
-    console.log(result)
-    let ranges = result.payload.blame.ranges;
+function getLineBlameCommitHash(blameResponse, line){
+    let ranges = blameResponse.payload.blame.ranges;
     for(let startLineStr in ranges){
         let startLine = Number(startLineStr);
         let range = ranges[startLine];
         if(range.end >= line){
-            return JSON.parse(JSON.stringify(ranges[startLine])).commitOid;
+            return JSON.parse(JSON.stringify(ranges[startLine].commitOid));
         }
     }
     return Promise.reject("couldnt find line");
 }
 
-async function getPreviousCommit(user, repo, commit_hash){
+function getBlameCommitUser(blameResponse, commitHash){
+    let commit = blameResponse.payload.blame.commits[commitHash];
+    return {name: commit.commiterName, img: commit.authorAvatarUrl};
+}
+
+async function getCommit(user, repo, commit_hash){
     let response = await fetch(`https://api.github.com/repos/${user}/${repo}/commits/${commit_hash}`);
     let result = await response.json();
-    console.log(result)
+    return result;
 }
 
 async function getPullRequest(user, repo, commit_hash){
     let response = await fetch(`https://api.github.com/repos/${user}/${repo}/commits/${commit_hash}/pulls`)
     let result = await response.json();
-    console.log(result[0].number);
     return result[0].number;
 }
 
@@ -98,4 +120,24 @@ async function getApprovingReviwers(user, repo, pr_num){
     return result.map(r => {
         return {img: r.user.avatar_url, name: r.user.login}
     });
+}
+
+async function getPreviousAuthors(blame, user, repo, path, commitHash, line){
+    let authors = [];
+
+    for(let i = 0; i < 3; i++){
+        // Get the commit data to get the parent commit hash
+        let commit = await getCommit(user, repo, commitHash);
+        authors.push( {img: commit.author.avatar_url, name: commit.author.login} );
+
+        if(commit.parents.length == 0){
+            break;
+        }
+        let parentCommitHash = commit.parents[0].sha;
+
+        // Get the blame for the parent commit to get the last commit on the line
+        blame = await getBlame(user, repo, parentCommitHash, path, line);
+        commitHash = getLineBlameCommitHash(blame, line);
+    }
+    return authors;
 }
